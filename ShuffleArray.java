b2c8d314692a30715356a26f9a2e77ec0403cf18
Rishabh Sharma <rishabhsharma@gmail.com>
2021-06-26 12:02:18 +0530

    final Prgrams

---
+++ ShuffleArray.java
@@ -0,0 +1,14 @@
+class ShuffleArray {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2*n];
+        for(int i=0;i<result.length;i++)
+        {
+            if(i%2==0)
+                result[i]=nums[i/2];
+            else
+                result[i]=nums[n+i/2];
+        }
+
+    return result;
+    }
+    }
